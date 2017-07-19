78734543332242d78ca13687533e7f88f872454d
Alfonso Rios <malforime@gmail.com>
2017-07-19 18:25:13 -0500

    Se pueden eliminar búsquedas

--- js/main.js
+++ js/main.js
@@ -1,31 +1,78 @@
 'use strict';
+// Create a last element property for arrays.
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
 
 var recentSearches = [];
-var originaList = document.getElementById('list-searches');
 
 /**
- * @description This method saves in the array recentSearches a search.
- * @param none
+ * @description This method saves in the array {recentSearches} a search.
  */
 function saveSearch() {
     var search = document.getElementById('input-looking-for').value;
-    recentSearches.push({id: recentSearches.length + 1, data: search});
+    if (recentSearches.length !== 0) {
+        recentSearches.push({id: recentSearches.last().id + 1, data: search});
+    } else {
+        recentSearches.push({id: 0, data: search});
+    }
     document.getElementById('input-looking-for').value = '';
 }
 
+/**
+ * @description Show the div with the recomendations.
+ */
 function showRecentSearches() {
+    var textArea = document.getElementById('text-area');
+    var sizeRecentSearches = recentSearches.length;
+    if (sizeRecentSearches >= 1) {
+        textArea.classList.remove('hidden');
+        const heightByItem = 30;
+        textArea.style.height = (20 + (heightByItem * sizeRecentSearches)).toString() + 'px';
+    }
     var list = document.getElementById('list-searches');
     recentSearches.forEach(function (iterator) {
-        addElement(list, iterator);
+        addSearchItem(list, iterator);
     });
 }
 
-function addElement (listElement, valueElement) {
-    // create a new li element
-    // and give it some content
+/**
+ * @description hide the div when losses the focus.
+ */
+function hideRecentSearches() {
+    setTimeout(function() {
+        var textArea = document.getElementById('text-area');
+        var list = document.getElementById('list-searches');
+        textArea.classList.add('hidden');
+        list.innerHTML = '';
+    }, 300);
+}
+
+/**
+ * @description Insert a li web element into the ul (list of searches)
+ * @param listElement: {id: number, data: string}[] - Array with the recentSearches
+ * @param valueElement: The item iterator has display value and id for delete it.
+ */
+function addSearchItem (listElement, valueElement) {
+    var link = '&#09; <a href="#" onclick="removeSearch(' + valueElement.id + ')">Eliminar</a>';
     var newLi = document.createElement('li');
     var newContent = document.createTextNode(valueElement.data);
-    var newHref = document.create
-    newLi.appendChild(newContent); //add the text node to the newly created li.
+    newLi.appendChild(newContent);
+    newLi.innerHTML += link;
     listElement.appendChild(newLi);
+}
+
+/**
+ * @description Removes the item was selected.
+ * @param id {number} - Id of recent searches.
+ */
+function removeSearch(id) {
+    for(var i = 0; i < recentSearches.length; i++) {
+        if (recentSearches[i].id == id) {
+            delete recentSearches[i];
+            break;
+        }
+    }
 }
